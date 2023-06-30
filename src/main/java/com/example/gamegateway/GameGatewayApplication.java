09cc380bb25aa692ffa7de45aae88bd00ea867f2
Thilina-Pathirage <thilinapathirage@gmail.com>
2023-06-30 16:54:16 +0530

    update

--- src/main/java/com/example/gamegateway/GameGatewayApplication.java
+++ src/main/java/com/example/gamegateway/GameGatewayApplication.java
@@ -4,30 +4,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.util.pattern.PathPatternParser;
-import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GameGatewayApplication implements WebFluxConfigurer {
+public class GameGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GameGatewayApplication.class, args);
     }
 
     @Bean
-    public CorsWebFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*")); // 1
+        corsConfig.setMaxAge(3600L); // 2
+        corsConfig.setAllowedMethods(Collections.singletonList("*")); // 3
+        corsConfig.setAllowedHeaders(Collections.singletonList("*")); // 4
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
-        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration("/**", corsConfig); // 5
 
         return new CorsWebFilter(source);
     }
