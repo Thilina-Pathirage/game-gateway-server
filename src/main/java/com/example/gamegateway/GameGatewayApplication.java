67c30b7c0d10f09513b5e39ddc2ddc08f80e34bb
Thilina-Pathirage <thilinapathirage@gmail.com>
2023-06-30 12:32:52 +0530

    update

--- src/main/java/com/example/gamegateway/GameGatewayApplication.java
+++ src/main/java/com/example/gamegateway/GameGatewayApplication.java
@@ -3,13 +3,32 @@ package com.example.gamegateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.util.pattern.PathPatternParser;
+import org.springframework.web.cors.CorsConfiguration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GameGatewayApplication {
+public class GameGatewayApplication implements WebFluxConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(GameGatewayApplication.class, args);
     }
 
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
 }
